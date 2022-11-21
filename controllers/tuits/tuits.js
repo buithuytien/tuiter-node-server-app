ce223fda90221395980288c683035683b70f55f3
buithuytien <buithuytien13.13@gmail.com>
2022-11-20 19:53:31 -0500

    a8 username to userName

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,6 +1,6 @@
 export default [
     {
-        "_id": 123, "topic": "Space", "username": "SpaceX",
+        "_id": 123, "topic": "Space", "userName": "SpaceX",
         "handle": "@spacex", "time": "2h", "image": "spacex.png",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
@@ -9,7 +9,7 @@ export default [
         "replies": 123, "retuits": 432
     },
     {
-        "_id": 234, "topic": "Traffic", "username": "The Boring Company",
+        "_id": 234, "topic": "Traffic", "userName": "The Boring Company",
         "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
